3b22e41cedb17cbd6d4983a1901f7ee1bb1410b8
Chang-Jin <2475477667@qq.com>
2019-11-18 16:30:00 +0800

    docs: 添加vdom中patch函数注释

--- vue-dev/src/platforms/web/runtime/patch.js
+++ vue-dev/src/platforms/web/runtime/patch.js
@@ -1,12 +1,13 @@
 /* @flow */
 
-import * as nodeOps from 'web/runtime/node-ops'
+import * as nodeOps from 'web/runtime/node-ops' // nodeOps封装了一些对原生dom操作的方法 Chang-Jin 2019-11-18
 import { createPatchFunction } from 'core/vdom/patch'
-import baseModules from 'core/vdom/modules/index'
+import baseModules from 'core/vdom/modules/index' // directives和ref属性的处理 Chang-Jin 2019-11-18
 import platformModules from 'web/runtime/modules/index'
 
 // the directive module should be applied last, after all
 // built-in modules have been applied.
-const modules = platformModules.concat(baseModules)
+// 指令模块应该在所有内置模块之后应用 Chang-Jin 2019-11-18
+const modules = platformModules.concat(baseModules) // 对一些特殊内容的特殊处理 Chang-Jin 2019-11-18
 
 export const patch: Function = createPatchFunction({ nodeOps, modules })
